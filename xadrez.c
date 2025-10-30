f11d0e5110d56c2e410eb69640eeb509e81aba4d
v-machado08 <vitinmachadu@gmail.com>
2025-10-30 14:56:27 +0000

    Adicionei o movimento do Cavalo, com estrutura aninhada While, for
    Falta comentar

--- xadrez.c
+++ xadrez.c
@@ -8,13 +8,16 @@ int main() {
     int torre;
     int bispo, w1=0;
     int rainha, r1=0;
+    int cavalo, c1=1;
     // o bloco abaixo mostra ao usuário como funcionará a movimentação da torre
     printf("Movimentando as peças de xadrez !\n");
     printf("A TORRE move-se em linha vertical ou horizontal, por favor escolha a direção do movimento da torre:\n");
-    printf("(Direita, esquerda, frente ou trás)\n)");
+    printf("(Direita, esquerda, frente ou trás)\n");
+    printf("\n");
 
     // cria o menu que solicita ao usuário a direção que gostaria de movimentar a torre
     printf("********** Escolha a direção que você gostaria de movimentar a torre*********\n");
+    printf("\n");
     printf("1- DIREITA\n");
     printf("2- ESQUERDA\n");
     printf("3- FRENTE\n");
@@ -58,6 +61,7 @@ int main() {
 // cria o menu que solicita ao usuário a direção que gostaria de movimentar O BISPO
     printf("O BISPO move-se em diagonal, por favor, escolha a direção do movimento do bispo\n");
     printf("********** Escolha a direção que você gostaria de movimentar o bispo*********\n");
+    printf("\n");
     printf("1- CIMA, DIREITA\n");
     printf("2- CIMA, ESQUERDA\n");
     printf("3- BAIXO,DIREITA\n");
@@ -104,7 +108,8 @@ int main() {
     
 // cria o menu que solicita ao usuário a direção que gostaria de movimentar a RAINHA
     printf("A RAINHA move-se em todas as direçoes, por favor, escolha a direção do movimento da Rainha\n");
-    printf("********** Escolha a direção que você gostaria de movimentar a Rainha*********\n");
+    printf("********** Escolha a direção que você gostaria de movimentar o bispo*********\n");
+    printf("\n");
     printf("1- CIMA, DIREITA\n");
     printf("2- CIMA, ESQUERDA\n");
     printf("3- BAIXO,DIREITA\n");
@@ -191,9 +196,145 @@ int main() {
             printf("Opção Inválida\n"); // caso o usuário escolha uma opção inválida (fora de 1 a 4)
             break;
 
-
-
     }
+
+        // o bloco abaixo mostra ao usuário como funcionará a movimentação do cavalo
+    printf("O cavalo movimenta-se em L, por favor escolha a direção do movimento do cavalo:\n");
+    printf("\n");
+    printf("1- CIMA, CIMA, DIREITA\n");
+    printf("2- CIMA, CIMA, ESQUERDA\n");
+    printf("3- BAIXO, BAIXO, DIREITA\n");
+    printf("4- BAIXO, BAIXO, ESQUERDA\n");
+    printf("5- DIREITA, DIREITA, CIMA\n");
+    printf("6- DIREITA, DIREITA, BAIXO\n");
+    printf("7- ESQUERDA, ESQUERDA, CIMA\n");
+    printf("8- ESQUERDA, ESQUERDA, BAIXO\n");
+    printf("9- BAIXO, BAIXO, DIREITA\n");
+    printf("10- BAIXO, BAIXO, ESQUERDA\n");
+    scanf("%d", &cavalo);
+
+    switch (cavalo) {
+
+        case 1:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("CIMA \n");
+                    }
+            
+            printf("DIREITA\n");}
+        
+        break;
+
+
+        case 2:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("CIMA \n");
+                    }
+            
+            printf("ESQUERDA\n");}
+        
+        break;
+
+
+
+        case 3:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("BAIXO \n");
+                    }
+            
+            printf("DIREITA\n");}
+        break;
+
+
+        case 4:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("BAIXO \n");
+                    }
+            
+            printf("ESQUERDA\n");}
+        break;
+
+
+         case 5:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("DIREITA \n");
+                    }
+            
+            printf("CIMA\n");}
+        break;
+
+
+        case 6:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("DIREITA \n");
+                    }
+            
+            printf("BAIXO\n");}
+        break;
+
+
+        case 7:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("ESQUERDA \n");
+                    }
+            
+            printf("CIMA\n");}
+        break;
+
+
+        case 8:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("ESQUERDA \n");
+                    }
+            
+            printf("BAIXO\n");}
+        break;
+
+
+        case 9:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("BAIXO \n");
+                    }
+            
+            printf("DIREITA\n");};
+        break;
+
+
+        case 10:
+            while (c1 -- ) {
+                for (int i =0; i < 2; i++ ){
+
+                   printf("BAIXO \n");
+                    }
+            
+            printf("ESQUERDA\n");}
+        
+        break;
+
+        default:
+            printf("Opção Inválida\n"); // caso o usuário escolha uma opção inválida (fora de 1 a 4)
+        break;
+        
+        }
+
+
 
     
 
